19c1386d44ac343dd8a2f5ffec59e8b90e5dba67
Sushant S <32960414+Sushants278@users.noreply.github.com>
2023-09-05 09:38:36 +0200

    Created headerView to show segment selection view

---
+++ My Posts/Interface/Posts/View/UserPostsHeaderView.swift
@@ -0,0 +1,54 @@
+//
+//  UserPostsHeaderView.swift
+//  My Posts
+//
+//  Created by Sushant Shinde on 04.09.23.
+//
+
+import UIKit
+
+protocol HeaderViewDelegate: AnyObject {
+    
+    func segmentedControlValueChanged(_ selectedIndex: Int)
+}
+
+class UserPostsHeaderView: UIView {
+    
+    weak var delegate: HeaderViewDelegate?
+    
+    let segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["All", "Favorites"])
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.selectedSegmentIndex = 0 // Set the initial selected segment
+        return segmentedControl
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        backgroundColor = .white
+        
+        addSubview(segmentedControl)
+        
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged), for: .valueChanged)
+        
+        NSLayoutConstraint.activate([
+            segmentedControl.centerXAnchor.constraint(equalTo: centerXAnchor),
+            segmentedControl.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    @objc private func segmentedControlValueChanged() {
+        delegate?.segmentedControlValueChanged(segmentedControl.selectedSegmentIndex)
+    }
+}
+
